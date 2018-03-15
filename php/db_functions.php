7f81abd2aebce75b7cf1224c7517e7b1ac0deafb
Laars1 <33323285+Laars1@users.noreply.github.com>
2018-03-15 16:09:11 +0100

    kill me please

--- php/db_functions.php
+++ php/db_functions.php
@@ -7,4 +7,19 @@
  *  sqlQuery() und sqlSelect() aus dem Modul basic_functions.php auf.
  */
 
+
+function db_insert_Users($params) {
+    $sql = "insert into Users (Benutzername, Email, Passwort)
+            values ('".escapeSpecialChars($params['benutzername'])."','".escapeSpecialChars($params['email'])."',
+					'".escapeSpecialChars($params['passwort'])."')";
+    sqlQuery($sql);
+}
+
+function db_select_User($params) {
+    $sql = "select *
+			from kontakte 
+			where Benutzername ='".escapeSpecialChars($params['benutzername'])."';";
+    return sqlSelect($sql);
+}
+
 ?>
